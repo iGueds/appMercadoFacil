1d4a4ac7abb2c73ce34028c5397b7edeecc1e608
IGuedes <icarogsantos@gmail.com>
2019-04-17 17:35:58 -0300

    AC-05

--- app/src/main/java/com/example/appmercadofacil/LoginActivity.kt
+++ app/src/main/java/com/example/appmercadofacil/LoginActivity.kt
@@ -1,8 +1,13 @@
 package com.example.appmercadofacil
 
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v4.content.ContextCompat
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -21,6 +26,10 @@ class LoginActivity : AppCompatActivity() {
             val campoUsuario = user_login.text.toString()
             val valorSenha = user_password.text.toString()
 
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                window.setStatusBarColorTo(R.color.colorPrimary)
+            }
+
             if (campoUsuario == "aluno" && valorSenha == "impacta") {
                 val intent = Intent(this, TelaInicial::class.java)
                 val param = Bundle()
@@ -33,5 +42,12 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun Window.setStatusBarColorTo(color: Int){
+        this.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        this.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        this.statusBarColor = ContextCompat.getColor(baseContext, color)
     }
 }
